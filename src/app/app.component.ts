218f97fc9dd04ee6819674a2f546421cbc2b7653
krantzinator <krantzrs8@gmail.com>
2017-06-07 23:11:23 -0400

    laid base for use of forms input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,37 @@
 import { Component } from '@angular/core';
 
+export class List {
+  name: string;
+  topic: string;
+  item: {
+    name: string;
+    price: number;
+  };
+}
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+  <h1>{{list.name}}</h1>
+  <h2>{{list.topic}}</h2>
+  <div>
+    <label>item: </label>
+    <input [(ngModel)]="list.item.name" placeholder=name>
+  </div>
+    <div>
+    <label>item: </label>
+    <input [(ngModel)]="list.item.price" placeholder=price>
+  </div>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent  { 
+  list: List = {
+    name: 'Fancy Schmancy List',
+    topic: 'Groceries',
+    item: {
+      name: 'ginger snap ice cream',
+      price: 3.99
+    }
+  };
+ }
